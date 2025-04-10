5aa09d00ec0c31b07ecd8075946fb367f7dd6ffc
Datapunk <87508558+0xDatapunk@users.noreply.github.com>
2025-04-10 10:01:44 -0400

    deploy ponder

--- transfer+balanceOf/ponder/ponder.config.ts
+++ transfer+balanceOf/ponder/ponder.config.ts
@@ -15,14 +15,14 @@ export default createConfig({
       network: "mainnet",
       abi: LBTCAbi,
       address: "0x8236a87084f8B84306f72007F36F2618A5634494",
-      startBlock: 0,
+      startBlock: 22210000,
     }
   },
   blocks: {
     HourlyUpdate: {
       network: "mainnet",
       interval: 60 * 60 / 12, // Approximating hourly based on block time
-      startBlock: 0
+      startBlock: 22210000
     }
   },
 });
